1148409d6acdff4c75d1cdb3d6a1e44b6400ac18
David H <darvidian123456@gmail.com>
2024-01-24 00:35:16 -0500

    added code to check for bugs

---
+++ README.md
@@ -0,0 +1,36 @@
+# Honeycutt-React-Portfolio
+  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+  ## Description
+  
+  This is my Portfolio designed with React
+  
+  ## Table of Contents
+  
+  * [Usage](#usage)
+  
+  * [License](#license)
+
+  * [Contribution](#contribution)
+  
+  * [Questions](#questions)
+  
+  
+  ## Usage 
+  
+  This Portfolio is designed to be sent out to potential employers to show my contact information and previous projects I have worked on.
+  
+  ## License
+        This project is licensed under the MIT license.
+  
+  ## Contribution 
+  
+  If you see something you feel you can improve upon, create a pull request and I'll review it!
+  
+  
+  ## Questions 
+  
+  Have some questions? Send me an email at darvidian123456@gmail.com. Find more of my projects at [thefrenchman1](https://github.com/thefrenchman1/).
+  
+  Happy Coding! 
+  
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Unique Password Generator",
+    subtitle: "Javascript",
+    description:
+      "This was a project I worked on to create a unique password. It uses Javascript to create a password based on user input questions.",
+    image: "./password maker.gif",
+    link: "https://thefrenchman1.github.io/Password-maker-5000/",
+  },
+  {
+    title: "Event Center",
+    subtitle: "MERN and TailwindsCSS",
+    description:
+      "The EventCenter is a full-stack MERN application that I worked on with a team of 5 other developers. It allows users to create and view events.",
+    image: "Event-Center.gif",
+    link: "https://eventcenter.onrender.com/",
+  },
+  {
+    title: "Local Planting Planner",
+    subtitle: "Local Storage and API",
+    description:
+      "This was a project I worked on to learn about API's. It allows users to search for plants that will grow in their hardiness zone and save their previous searches.",
+    image: "./PlantingPlanner.gif",
+    link: "https://itsaboy.github.io/local-planting-planner/",
+  },
+  {
+    title: "Trail Buddy",
+    subtitle: "Sequalize and Express",
+    description:
+      "The TrailBuddy application was a project used for designing a social media type platform that allows users to log in and post blogs about their favorite trails.",
+    image: "./TrailBuddy.gif",
+    link: "https://trail-buddy-cary-nc-4eb34ba4009f.herokuapp.com/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "I had the pleasure of working with David on multiple projects during our time at UNC Chapel Hill. He is always willing to help and is a great team player. He is a great asset to any team.",
+    image: "Josiah-face.jpg",
+    name: "Josiah Smith",
+    company: "UNC Chapel Hill Alumni",
+  },
+  {
+    quote:
+      "I would highly recommend David for any professional role. He consistantly shows his problem-solving ability, attention to detail, and team dynamics in his work.",
+    image: "Amanda-face.jpg",
+    name: "Amanda Foley",
+    company: "Entrust",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "TailwindCSS",
+  "Node",
+  "HTML",
+  "Debugging",
+];
